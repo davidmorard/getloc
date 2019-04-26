f72c5de0462c209470ba03f4757e089d9efd32e0
davidmorard <davidmorard@gmail.com>
2019-04-26 18:52:37 +0200

    First install template

--- src/Controller/ClsController.php
+++ src/Controller/ClsController.php
@@ -5,15 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class BlogController extends AbstractController
+class ClsController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog")
+     * @Route("/cls", name="cls")
      */
     public function index()
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+        return $this->render('cls/index.html.twig', [
+            'controller_name' => 'ClsController',
         ]);
     }
 }
